ad810e7ea1c8aa8bcedf9397256507090bf1e5b2
Julian Rubisch <julian@julianrubisch.at>
2021-10-12 13:48:56 +0200

    Render original placeholder

--- lib/futurism_nice_partials/helper.rb
+++ lib/futurism_nice_partials/helper.rb
@@ -4,9 +4,17 @@ module FuturismNicePartials
 
     alias_method :orig_futurize, :futurize
 
-    def futurize(records_or_string = nil, placeholder:, extends:, **options, &block)
-      # render records_or_string, &block
-      orig_futurize(records_or_string, extends: extends, **options) { placeholder }
+    def futurize(records_or_string = nil, extends:, **options, &block)
+      renderer = PlaceholderRenderer.new
+      orig_futurize(records_or_string, extends: extends, **options) { block.call(renderer) }
     end
+  end
+
+  class PlaceholderRenderer
+    def placeholder(content = nil, options = {}, &block)
+      block.call
+    end
+
+    def method_missing(symbol, *args); end
   end
 end
