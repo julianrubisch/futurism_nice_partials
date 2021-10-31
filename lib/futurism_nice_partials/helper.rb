59db154c13908a6a2cb4fb4b1370f390c48c94a8
Julian Rubisch <julian@julianrubisch.at>
2021-10-31 20:41:53 +0100

    Use OptionsTransformer to restore partial key

--- lib/futurism_nice_partials/helper.rb
+++ lib/futurism_nice_partials/helper.rb
@@ -3,16 +3,25 @@ require "cable_ready/compoundable"
 module FuturismNicePartials
   module Helper
     include Futurism::Helpers
+    include Futurism::OptionsTransformer
     include CableReady::Compoundable
 
     alias_method :orig_futurize, :futurize
 
     def futurize(records_or_string = nil, extends:, **options, &block)
-      compound_key = compound([records_or_string, extends, *options.to_a])
-      FuturismNicePartials.store[compound_key] = ContextStoreItem.new(context: self, block: block.dup)
+      FuturismNicePartials.store[compound_key(records_or_string, extends: extends, **options)] = ContextStoreItem.new(context: self, block: block.dup)
 
       renderer = PlaceholderRenderer.new
       orig_futurize(records_or_string, extends: extends, **options) { block.call(renderer) }
+    end
+
+    def compound_key(records_or_string, extends:, **options)
+      if records_or_string.is_a?(String)
+        html_options = options.delete(:html_options)
+        compound({partial: records_or_string, locals: dump_options(options)})
+      else
+        compound(dump_options(options))
+      end
     end
   end
 
--- lib/futurism_nice_partials/resources_extension.rb
+++ lib/futurism_nice_partials/resources_extension.rb
@@ -1,5 +1,9 @@
+require "cable_ready/compoundable"
+
 module FuturismNicePartials
   module ResourcesExtension
+    include CableReady::Compoundable
+
     def resolve
       resolved_models.zip(@resources_with_sgids).each do |model, resource_definition|
         html = renderer_for(resource_definition: resource_definition).render(model)
@@ -8,12 +12,14 @@ module FuturismNicePartials
       end
 
       @resources_without_sgids.each do |resource_definition|
-        resource = lookup_resource(resource_definition)
+        options = options_from_resource(resource_definition)
         renderer = renderer_for(resource_definition: resource_definition)
         html =
           begin
-            partial = resource.delete(:partial)
-            renderer.render(partial, resource_definition.controller.new.view_context, nil).body
+            store_item = FuturismNicePartials.store[compound(dump_options(options))]
+
+            partial = options.delete(:partial)
+            renderer.render(partial, store_item.context, store_item.block).body
           rescue => exception
             error_renderer.render(exception)
           end
@@ -23,10 +29,10 @@ module FuturismNicePartials
     end
 
     def renderer_for(resource_definition:)
-      options = lookup_resource(resource_definition)
+      options = options_from_resource(resource_definition)
 
       @lookup_context = ActionView::LookupContext.new(ActionController::Base.view_paths)
-      @renderer ||= ActionView::PartialRenderer.new(@lookup_context, options)
+      ActionView::PartialRenderer.new(@lookup_context, options)
     end
   end
 end
